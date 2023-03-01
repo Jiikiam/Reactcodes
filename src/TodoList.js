4345d99d183c61ba4faaa3a7bba03d1995622407
Jiikiam <leinonen.joakim@gmail.com>
2023-03-01 12:05:42 +0200

    Todolist with delete-button

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,42 +1,60 @@
-import './App.css';
 import React, { useState } from 'react';
+import './App.css';
 
 function App() {
-    const [desc, setDesc] = useState('');
-    const [todos, setTodos] = useState([]);
-    const [date, setDate] = useState('');
+  const [desc, setDesc] = useState('');
+  const [date, setDate] = useState('');
+  const [todos, setTodos] = useState([]);
 
-    const inputChanged = (event) => {
-        setDesc(event.target.value);
-    }
-    const inputChange = (event) => {
-        setDate(event.target.value);
-    }
-    const addTodo = (event) => {
-        event.preventDefault();
-        setTodos([...todos, desc + " " + date]);
-    }
-    return (
+  const inputChanged = (event) => {
+    setDesc(event.target.value);
+  };
 
-        <div className="App">
-            <form onSubmit={addTodo}>
-                <p>Description:<input type="text" onChange={inputChanged} value={desc} /> Date: <input type="text" onChange={inputChange} value={date} /></p>
-                <button onClick="addTodo">Add</button>
-            </form>
-            <table>
-                <tbody>
-                    <p>Date Description</p>
-                    {
-                        todos.map((todo, index) =>
-                            <tr key={index}>
-                                <td>{todo}</td>
-                            </tr>
-                        )
-                    }
-                </tbody>
-            </table>
-        </div>
-    );
+  const dateChanged = (event) => {
+    setDate(event.target.value);
+  };
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, date + " " + desc]);
+  };
+
+  const deleteTodo = (index) => {
+    setTodos(todos.filter((_, i) => i !== index));
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={addTodo}>
+        <p>
+          Description:
+          <input type="text" onChange={inputChanged} value={desc} />
+          Date:
+          <input type="text" onChange={dateChanged} value={date} />
+          <button type="submit">Add</button>
+        </p>
+      </form>
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todos.map((todo, index) => (
+            <tr key={index}>
+              <td>{todo.split(" ")[0]}</td>
+              <td>{todo.split(" ")[1]}</td>
+              <td>
+                <button onClick={() => deleteTodo(index)}>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default App;
